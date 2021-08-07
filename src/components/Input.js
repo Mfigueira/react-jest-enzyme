656d76b4bc501fae6a507df78ea06b1fe5851137
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-08-07 14:02:28 -0300

    added guessForm test

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,35 @@
-const Input = () => {
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  const handleInputChange = e => {
+    setCurrentGuess(e.target.value);
+  };
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    setCurrentGuess('');
+  };
+
   return (
     <div data-test="component-input">
-      <h1>input</h1>
+      <Form data-test="guess-form" onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Control
+            data-test="input-box"
+            type="text"
+            placeholder="type a word..."
+            value={currentGuess}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Button data-test="submit-button" variant="primary" type="submit">
+          Guess!
+        </Button>
+      </Form>
     </div>
   );
 };
--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,6 +1,14 @@
+import React from 'react';
 import { shallow } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import Input from './Input';
+
+// Option for mocking a hook that allows destructured imports on the component
+// const mockSetCurrentGuess = jest.fn();
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useState: state => [state, mockSetCurrentGuess],
+// }));
 
 const renderInput = () => shallow(<Input />);
 
@@ -8,4 +16,32 @@ test('renders without error', () => {
   const wrapper = renderInput();
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
+});
+
+describe('state controlled input filed', () => {
+  let mockSetCurrentGuess;
+  let wrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    wrapper = renderInput();
+  });
+
+  test('state is updated with the correct value from input', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('state is cleared on form submit', () => {
+    const guessForm = findByTestAttr(wrapper, 'guess-form');
+
+    guessForm.simulate('submit', { preventDefault() {} });
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
+  });
 });
