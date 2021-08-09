853c08faba75ae2d7096f147389ec9466ca6c7e9
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-08-09 10:31:45 -0300

    added correctGuess action creator and test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+};
+
 export const getSecretWord = async () => {
   const { data } = await axios.get('http://localhost:3030');
   return data;
 };
+
+export const correctGuess = () => ({
+  type: actionTypes.CORRECT_GUESS,
+});
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,5 +1,5 @@
 import moxios from 'moxios';
-import { getSecretWord } from '.';
+import { getSecretWord, correctGuess, actionTypes } from '.';
 
 describe('getSecretWord', () => {
   beforeEach(() => {
@@ -21,5 +21,12 @@ describe('getSecretWord', () => {
 
     const secretWord = await getSecretWord();
     expect(secretWord).toBe('party');
+  });
+});
+
+describe('correctGuess', () => {
+  test(`returns an object with type ${actionTypes.CORRECT_GUESS}`, () => {
+    const action = correctGuess();
+    expect(action).toStrictEqual({ type: actionTypes.CORRECT_GUESS });
   });
 });
